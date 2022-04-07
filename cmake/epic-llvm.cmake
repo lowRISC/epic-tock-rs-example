5d9254a2faec6ea49dceaad12e180e001ce16882
Jorge Prendes <jorge.prendes@gmail.com>
2022-04-07 14:59:16 +0100

    Build LLVM and Rust with RISC-V relaxation support

--- cmake/epic-llvm.cmake
+++ cmake/epic-llvm.cmake
@@ -5,6 +5,7 @@ if ( NOT LLVM_DIR )
 ExternalProject_Add(epic-llvm
     URL https://github.com/jprendes/llvm-project/archive/refs/heads/epic2.zip
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/llvm
+    PATCH_COMMAND git apply -p0 ${CMAKE_CURRENT_SOURCE_DIR}/relaxations.patch
     CMAKE_ARGS 
         ${CMAKE_CURRENT_BINARY_DIR}/llvm/src/epic-llvm/llvm
         -G Ninja
